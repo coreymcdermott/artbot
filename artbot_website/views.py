c584cb5a21c648523276604734d87d93e2de04df
Corey McDermott <corey@mcdermott.id.au>
2015-11-24 00:11:55 +1100

    Fixed Index view query.
    Index view now lists events that are open on the coming Saturday but
    closed on the coming Sunday.

--- artbot_website/views.py
+++ artbot_website/views.py
@@ -4,6 +4,5 @@ from .models          import Event
 
 def index(request):
     weekend_start = date.today()  + timedelta((6 - date.today().isoweekday()) % 7 )
-    weekend_end   = weekend_start + timedelta(1)
-    events = Event.objects.filter(start__lte = weekend_start, end__gte = weekend_end).order_by('-start')
+    events = Event.objects.filter(start__lte = weekend_start, end__gte = weekend_start).order_by('-start')
     return render(request, 'index.html', {'events': events})

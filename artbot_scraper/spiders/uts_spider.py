aabd75381775fceeee7aff1560d5d6ea3d7bc375
Corey McDermott <corey@mcdermott.id.au>
2015-11-24 22:24:04 +1100

    Added UTS spider.

---
+++ artbot_scraper/spiders/uts_spider.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+import re
+from scrapy               import Spider, Request
+from dateutil             import parser, relativedelta
+from artbot_scraper.items import EventItem
+
+class UTSSpider(Spider):
+    name            = "UTS"
+    allowed_domains = ["art.uts.edu.au"]
+    start_urls      = ["http://art.uts.edu.au/index.php/exhibitions/"]
+
+    def parse(self, response):
+        for href in response.xpath('//a[contains(@class, "listed-item-link")]/@href'):
+            url = response.urljoin(href.extract())
+
+            yield Request(url, callback=self.parse_exhibition)
+
+    def parse_exhibition(self, response):
+        item = EventItem()
+        item['url']         = response.url
+        item['venue']       = "UTS"
+        item['title']       = response.xpath('//h1[contains(@class, "entry-title")]/text()').extract_first().strip() \
+                            + " - " \
+                            + response.xpath('//h2[contains(@class, "entry-subtitle")]/text()').extract_first().strip()
+        item['description'] = ''.join(response.xpath('//div[contains(@class, "entry-content")]//text()').extract()).strip()
+        item['image']       = response.xpath('//img[contains(@class, "large-crop")]/@src').extract_first()
+        item['start']       = parser.parse(response.xpath('//span[contains(@class, "start-date")]/text()').extract_first().strip(), fuzzy = True)
+        item['end']         = parser.parse(response.xpath('//span[contains(@class, "end-date")]/text()').extract_first().strip(), fuzzy = True)
+
+        yield item

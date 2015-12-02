7003608f020b2176fb9ddb4c7edbd448cd81d395
Corey McDermott <corey@mcdermott.id.au>
2015-12-02 23:10:07 +1100

    Added event weekend and publication status filters.

--- artbot_website/admin.py
+++ artbot_website/admin.py
@@ -1,8 +1,31 @@
-from django.contrib import admin
-from .models        import Event
+from django.contrib       import admin
+from django.contrib.admin import SimpleListFilter
+from datetime             import date, datetime, timedelta
+from .models              import Event
+
+class EventWeekendFilter(SimpleListFilter):
+    title = ('weekend')
+    parameter_name = 'weekend'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('THIS_WEEKEND', ('This weekend')),
+            ('NEXT_WEEKEND', ('Next weekend')),
+        )
+
+    def queryset(self, request, queryset):
+
+        this_weekend = date.today() + timedelta((6 - date.today().isoweekday()) % 7 )
+        next_weekend = this_weekend + timedelta(days = 7)
+
+        if self.value() == 'THIS_WEEKEND':
+            return queryset.filter(start__lte = this_weekend, end__gte = this_weekend)
+        elif self.value() == 'NEXT_WEEKEND':
+            return queryset.filter(start__lte = next_weekend, end__gte = next_weekend)
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'venue', 'start', 'end', 'created')
+    list_filter   = ('published', EventWeekendFilter, )
+    list_display  = ('title', 'venue', 'start', 'end', 'created', 'published')
     search_fields = ('title', 'venue')
 
 admin.site.register(Event, EventAdmin)

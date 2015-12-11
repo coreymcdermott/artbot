61b4f4482ce108904c15e255cda9f2a7d1f2d671
Corey McDermott <corey@mcdermott.id.au>
2015-12-11 22:14:04 +1100

    Added 'crawlall' command.
    * Dynamically determines and runs all project spiders.
    * Run ‘scrapy crawl all’ from the project root directory.

---
+++ artbot_scraper/commands/crawlall.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from scrapy.commands      import ScrapyCommand
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler       import CrawlerProcess
+
+class Command(ScrapyCommand):
+    requires_project = True
+
+    def syntax(self):
+        return '[options]'
+
+    def short_desc(self):
+        return 'Run all project spiders'
+
+    def run(self, args, opts):
+        crawler_process = CrawlerProcess(get_project_settings())
+
+        for spider_name in crawler_process.spider_loader.list():
+            spider_cls = crawler_process.spider_loader.load(spider_name)
+            crawler_process.crawl(spider_cls)
+
+        crawler_process.start()
--- artbot_scraper/settings.py
+++ artbot_scraper/settings.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../../')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'artbot.settings'
 
+COMMANDS_MODULE  = 'artbot_scraper.commands'
 BOT_NAME         = 'artbot_scraper'
 SPIDER_MODULES   = ['artbot_scraper.spiders']
 NEWSPIDER_MODULE = 'artbot_scraper.spiders'

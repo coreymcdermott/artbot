5094a952edd9ffe954c497585357a46bef6d3505
Corey McDermott <corey@mcdermott.id.au>
2015-12-31 11:16:26 +1100

    Added crop and upload admin action.

--- artbot_website/admin.py
+++ artbot_website/admin.py
@@ -1,7 +1,8 @@
-from django.contrib       import admin
+from django.contrib       import admin, messages
 from django.contrib.admin import SimpleListFilter
 from datetime             import date, datetime, timedelta
 from .models              import Event
+
 
 class EventWeekendFilter(SimpleListFilter):
     title = ('weekend')
@@ -32,16 +33,25 @@ class EventWeekendFilter(SimpleListFilter):
 
 def publish(modeladmin, request, queryset):
     queryset.update(published = True)
-publish.short_description = "Publish selected events"
+publish.short_description = "Publish"
 
 def withdraw(modeladmin, request, queryset):
     queryset.update(published = False)
-withdraw.short_description = "Withdraw selected events"
+withdraw.short_description = "Withdraw"
+
+def crop_image(modeladmin, request, queryset):
+    for event in queryset:
+        try:
+            print event
+            event.crop_image()
+        except Exception as e:
+            messages.error(request, str(e))
+crop_image.short_description = "Crop and transload images"
 
 class EventAdmin(admin.ModelAdmin):
     list_filter   = ('published', EventWeekendFilter,)
     list_display  = ('title', 'venue', 'start', 'end', 'created', 'published')
     search_fields = ('title', 'venue')
-    actions       = [publish, withdraw,]
+    actions       = [publish, withdraw, crop_image]
 
 admin.site.register(Event, EventAdmin)
--- artbot_website/models.py
+++ artbot_website/models.py
@@ -1,5 +1,10 @@
+import urllib
+import boto3
+import os
 from django.db    import models
 from django.utils import timezone
+from PIL          import Image, ImageChops
+from StringIO     import StringIO
 
 class Event(models.Model):
     url         = models.TextField()
@@ -17,3 +22,34 @@ class Event(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def crop_image(self):
+
+        try:
+            image = Image.open(StringIO(urllib.urlopen(self.image).read()))
+        except:
+            raise
+        else:
+            image_out = StringIO()
+
+            border = Image.new(image.mode, image.size, image.getpixel((0,0)))
+            diff   = ImageChops.difference(image, border)
+            diff   = ImageChops.add(diff, diff, 2.0, -100)
+            bbox   = diff.getbbox()
+
+            if bbox:
+                image = image.crop(bbox)
+                image.save(image_out, 'JPEG')
+            else:
+                raise ValueError('Unable to determine image bounding box')
+
+            key = self.venue + '-' + self.description.replace(" ", "_") + '-' + self.start.strftime("%y-%m-%d") + '.jpg'
+            s3 = boto3.resource('s3')
+
+            try:
+                s3.Bucket(os.environ.get('AWS_S3_BUCKET')).put_object(Key = key, Body = image_out.getvalue(), ACL='public-read')
+            except:
+                raise
+            else:
+                self.image = 'https://s3-ap-southeast-2.amazonaws.com/artbot.io/' + key
+                self.save()

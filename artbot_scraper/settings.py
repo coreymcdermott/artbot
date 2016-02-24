43d1b74ba9b05d017a30e3c64f89d92411128a7f
Corey McDermott <corey@mcdermott.id.au>
2016-02-24 21:32:36 +1100

    Django 1.8.6 → 1.9.2.

--- artbot_scraper/settings.py
+++ artbot_scraper/settings.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import django
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'artbot.settings'
+
+django.setup()
 
 COMMANDS_MODULE  = 'artbot_scraper.commands'
 BOT_NAME         = 'artbot_scraper'

1c3e0474d5d8ae3375eb3ef5ff11cc7cafa536fb
Corey McDermott <corey@mcdermott.id.au>
2016-03-02 20:51:11 +1100

    Renamed scrape command "crawlall" to "all".

--- artbot_scraper/commands/all.py
+++ artbot_scraper/commands/all.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
 from scrapy.commands                import ScrapyCommand
-from scrapy.utils.project           import get_project_settings
-from scrapy.crawler                 import CrawlerProcess
 from scrapy.utils.log               import configure_logging
 from artbot_website.loggingHandlers import DatabaseLogHandler
 
 
 class Command(ScrapyCommand):
+
     requires_project = True
 
     def syntax(self):
@@ -17,15 +16,13 @@ class Command(ScrapyCommand):
         return 'Run all project spiders'
 
     def run(self, args, opts):
-        crawler_process = CrawlerProcess(get_project_settings())
-
         configure_logging(install_root_handler=False)
         logger = logging.getLogger()
         databaseLogHandler = DatabaseLogHandler()
         logger.addHandler(databaseLogHandler)
 
-        for spider_name in crawler_process.spider_loader.list():
-            spider_cls = crawler_process.spider_loader.load(spider_name)
-            crawler_process.crawl(spider_cls)
+        for spider_name in self.crawler_process.spider_loader.list():
+            spider_class = self.crawler_process.spider_loader.load(spider_name)
+            self.crawler_process.crawl(spider_class)
 
-        crawler_process.start()
+        self.crawler_process.start()

5b52251413fbb58e0cc5127fea8732b089020db8
Corey McDermott <corey@mcdermott.id.au>
2016-03-02 20:51:54 +1100

    Overriding standard scrape crawl command to ensure that database log handler is used.

---
+++ artbot_scraper/commands/crawl.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import logging
+from scrapy.commands                import ScrapyCommand
+from scrapy.exceptions              import UsageError
+from scrapy.utils.log               import configure_logging
+from artbot_website.loggingHandlers import DatabaseLogHandler
+
+
+class Command(ScrapyCommand):
+
+    requires_project = True
+
+    def syntax(self):
+        return "[options] <spider>"
+
+    def short_desc(self):
+        return "Run a spider"
+
+    def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError("running 'scrapy crawl' with more than one spider is no longer supported")
+        spider_name = args[0]
+
+        configure_logging(install_root_handler=False)
+        logger = logging.getLogger()
+        databaseLogHandler = DatabaseLogHandler()
+        logger.addHandler(databaseLogHandler)
+
+        self.crawler_process.crawl(spider_name)
+        self.crawler_process.start()

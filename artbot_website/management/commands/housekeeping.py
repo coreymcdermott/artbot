17d74db516ab71ecfcb24a4e07c3af44416815b9
Corey McDermott <corey@mcdermott.id.au>
2016-03-06 20:47:39 +1100

    Added housekeeping management command.
    * Deletes logs older than 3 days.

---
+++ artbot_website/management/commands/housekeeping.py
@@ -0,0 +1,15 @@
+from datetime                    import date, datetime, timedelta
+from django.core.management.base import NoArgsCommand, CommandError
+from artbot_website.models       import Event, Log
+# from django.conf                 import settings
+
+class Command(NoArgsCommand):
+    args = 'None.'
+    help = 'Performs housekeeping operations.'
+
+    def handle_noargs(self, *args, **options):
+        # Delete logs older than 3 days.
+        cutoff = datetime.now() - timedelta(days=3)
+        logs   = Log.objects.filter(timestamp__lt=cutoff)
+        logs.delete()
+        return

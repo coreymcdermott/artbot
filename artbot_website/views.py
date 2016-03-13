af0fa1e36c1ce3ff542f95daef86e45916f027d7
Corey McDermott <corey@mcdermott.id.au>
2016-03-14 00:02:08 +1100

    Fixed main view query timezone.
    * Now uses EST instead of UTC to determine current weekend.

--- artbot_website/views.py
+++ artbot_website/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from datetime         import date, datetime, timedelta
 from .models          import Event
+from pytz             import timezone
 
 
 def index(request):
-    if date.today().isoweekday() in [5, 6, 7]:
-        weekend_start = date.today()
+    now = datetime.now(timezone('Australia/Sydney')).date()
+    if now.isoweekday() in [5, 6, 7]:
+        weekend_start = now
     else:
-        weekend_start = date.today() + timedelta((5 - date.today().isoweekday()) % 7)
+        weekend_start = now + timedelta((5 - now.isoweekday()) % 7)
 
     events = Event.objects.filter(start__lte = weekend_start, end__gte = weekend_start, published = True).order_by('-start')
     return render(request, 'index.html', {'events': events})

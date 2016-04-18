f0ec3cd2c8bba2f929adbecea888ef9d5816ee0a
Corey McDermott <corey@mcdermott.id.au>
2016-04-18 21:04:20 +1000

    Added functionality to delete Django Admin logs older than retention period.

--- artbot_website/management/commands/housekeeping.py
+++ artbot_website/management/commands/housekeeping.py
@@ -2,15 +2,23 @@ import os
 from datetime                    import date, datetime, timedelta
 from django.core.management.base import BaseCommand, CommandError
 from artbot_website.models       import Event, Log
+from django.contrib.admin.models import LogEntry
+
 
 class Command(BaseCommand):
     args = 'None.'
     help = 'Performs housekeeping operations.'
 
     def handle(self, *args, **options):
-        # Delete logs older than retention period.
+        # Calculate retention cutofff.
         hours  = int(os.getenv('LOG_RETENTION', 24))
         cutoff = datetime.now() - timedelta(hours=hours)
-        logs   = Log.objects.filter(timestamp__lt=cutoff)
+
+        # Delete artbot_website logs older than retention period.
+        logs = Log.objects.filter(timestamp__lt=cutoff)
         logs.delete()
+
+        # Delete Django Admin logs older than retention period.
+        logEntries = LogEntry.objects.filter(action_time__lt=cutoff)
+        logEntries.delete()
         return

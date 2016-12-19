1b9cb274635cd0d305826f15410b05a424390c76
Corey McDermott <corey@mcdermott.id.au>
2016-12-19 21:09:29 +1100

    Fixed Galerie pompom spider post site change.

--- artbot_scraper/spiders/pompom_spider.py
+++ artbot_scraper/spiders/pompom_spider.py
@@ -11,19 +11,16 @@ class PompomSpider(CrawlSpider):
     name            = 'Galerie pompom'
     allowed_domains = ['galeriepompom.com']
     start_urls      = ['http://galeriepompom.com/galerie-pompom--exhibitions.html']
-    rules           = (Rule(LinkExtractor(allow=('2016', )), callback='parse_exhibition'),)
+    rules           = (Rule(LinkExtractor(deny=('exhibitions-2016', ), allow=('2016', )), callback='parse_exhibition'),)
 
     def parse_exhibition(self, response):
         item          = EventItem()
         item['url']   = response.url
         item['venue'] = self.name
-        item['title'] = response.xpath('//div[@id = "page"]/div[3]/p[1]/text()').extract_first().strip() + ' - ' +\
-                        response.xpath('//div[@id = "page"]/div[3]/p[2]/text()').extract_first().strip()
+        item['title'] = response.xpath('//div[contains(@data-muse-type, "txt_frame")]/p[2]/text()').extract_first().strip()
         item['image'] = response.urljoin(response.xpath('//div[contains(@class, "SlideShowWidget")]//img/@data-src').extract_first())
 
-        season = response.xpath('//div[@id = "page"]/div[3]/p[3]/text()').extract_first().strip()
-        if not season:
-            season = response.xpath('//div[@id = "page"]/div[3]/p[4]/text()').extract_first().strip()
+        season = response.xpath('//div[contains(@data-muse-type, "txt_frame")]/p[1]/text()').extract_first().strip()
         match  = re.match('(?P<start>[\d+\s+\w+]*)[\s\-]*(?P<end>\d+\s+\w+\s+\d+)', season)
 
         if (match):

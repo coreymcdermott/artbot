d59f9ced1262784ac727ee4c01da78ed9b6c1246
Corey McDermott <corey@mcdermott.id.au>
2016-12-20 22:46:30 +1100

    Fixed Verge Gallery post site change.

--- artbot_scraper/spiders/verge_gallery.py
+++ artbot_scraper/spiders/verge_gallery.py
@@ -10,36 +10,22 @@ class VergeGallerySpider(CrawlSpider):
     name            = 'Verge Gallery'
     allowed_domains = ['verge-gallery.net']
     start_urls      = ['https://verge-gallery.net/category/current/']
-    rules           = (Rule(LinkExtractor(allow=('/\d+\/\d+\/\d+'), deny=('eoi')), callback='parse_exhibition'),)
+    rules           = (Rule(LinkExtractor(allow=('/\d+\/\d+\/\d+'), deny=('eoi', '2017-program')), callback='parse_exhibition'),)
 
     def parse_exhibition(self, response):
-        item_one = EventItem()
-        item_two = EventItem()
+        item                = EventItem()
+        item['url']         = response.url
+        item['venue']       = self.name
+        item['title']       = response.xpath('//h2[contains(@class, "entry-title")]/text()').extract_first().strip()
+        item['description'] = response.xpath('//hr//following-sibling::p[1]//text()').extract_first().strip()
+        item['image']       = response.xpath('//figure[contains(@class, "featured-image")]//img/@src').extract_first().strip()
 
-        season   = response.xpath('//p[contains(@style, "outline: none;")]/text()').extract_first()
+        season = response.xpath('//h2[contains(@class, "entry-title")]/text()').extract_first()
+        match  = re.search('(?P<start>(?<=\:\:\s)\w+\s+\d+)[\s\-\–]*(?P<end>\w+\s+\d+)', season, re.UNICODE)
 
-        if season is not None:
-            match = re.search('(?P<start>(?<=OPENING\s)\w+\s+\d+).*(?P<end>(?<=CONTINUING TO\s)\w+\s+\d+)', season, re.MULTILINE)
+        if (match):
+            tz = timezone('Australia/Sydney')
+            item['start'] = tz.localize(parser.parse(match.group('start')))
+            item['end']   = tz.localize(parser.parse(match.group('end')))
 
-            if (match):
-                tz = timezone('Australia/Sydney')
-                item_one['start'] = tz.localize(parser.parse(match.group('start')))
-                item_one['end']   = tz.localize(parser.parse(match.group('end')))
-                item_two['start'] = tz.localize(parser.parse(match.group('start')))
-                item_two['end']   = tz.localize(parser.parse(match.group('end')))
-
-        item_one['url']         = response.url
-        item_one['venue']       = self.name
-        item_one['title']       = response.xpath('//hr//following-sibling::p[1]/text()').extract()[0].strip()
-        item_one['description'] = response.xpath('//hr//following-sibling::p[1]/following-sibling::p[1]/text()').extract()[0].strip()
-        item_one['image']       = response.xpath('//hr//following-sibling::p[1]/following-sibling::div[1]//img/@src').extract()[0].strip()
-
-        yield item_one
-
-        item_two['url']         = response.url
-        item_two['venue']       = self.name
-        item_two['title']       = response.xpath('//hr//following-sibling::p[1]/text()').extract()[1].strip()
-        item_two['description'] = response.xpath('//hr//following-sibling::p[1]/following-sibling::p[1]/text()').extract()[1].strip()
-        item_two['image']       = response.xpath('//hr//following-sibling::p[1]/following-sibling::div[1]//img/@src').extract()[1].strip()
-
-        yield item_two
+        yield item
